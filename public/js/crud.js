8cbea017a34c6897458d0e2d170c10177738b142
Nukecola2005 <williams.pesic@gmail.com>
2024-04-11 10:39:50 +0200

    REFACTOR: Crud with better ui elements

--- public/js/crud.js
+++ public/js/crud.js
@@ -1,4 +1,3 @@
-const elementList = document.querySelector(".element-list");
 const serverPull = "/getElements";
 const container = document.querySelector(".container")
 const contentContainer = document.querySelector(".content-container")
@@ -27,59 +26,73 @@ async function getPool() {
             headers: {
                 'Content-Type': 'application/json',
             },
-        })
+        });
 
         if (response.ok) {
             const data = await response.json();
             const pool = data.pool;
 
-            console.log(pool)
+            const wrapper = document.querySelector(".stacked-list1_list-wrapper");
 
             pool.forEach(elementData => {
-                const wrapper = document.querySelector(".stacked-list1_list-wrapper");
-
                 const item = document.createElement("div");
                 item.classList.add("stacked-list1_item");
-
+            
                 const avatar = document.createElement("div");
                 avatar.classList.add("stacked-list1_avatar");
-
+            
                 const avatarImageWrapper = document.createElement("div");
                 avatarImageWrapper.classList.add("stacked-list1_avatar-image-wrapper");
-
+            
                 const img = document.createElement("img");
                 img.src = elementData.URL;
                 img.loading = "lazy";
                 img.alt = elementData.Name;
                 img.classList.add("stacked-list1_avatar-image");
                 avatarImageWrapper.appendChild(img);
-
+            
                 const maxwidthLarge = document.createElement("div");
                 maxwidthLarge.classList.add("max-width-large");
-
+            
                 const itemName = document.createElement("div");
                 itemName.classList.add("text-weight-semibold");
                 itemName.textContent = elementData.Name;
                 maxwidthLarge.appendChild(itemName);
-
+            
                 avatar.appendChild(avatarImageWrapper);
                 avatar.appendChild(maxwidthLarge);
-
+            
                 const contentRight = document.createElement("div");
                 contentRight.classList.add("stacked-list1_content-right");
-
+            
                 const dropdownComponent = document.createElement("div");
                 dropdownComponent.classList.add("dropdown1_component", "w-dropdown");
-
+                dropdownComponent.setAttribute("data-hover", "false");
+                dropdownComponent.setAttribute("data-delay", "200");
+                dropdownComponent.setAttribute("data-w-id", "e27fbb5a-82f5-b6dd-7c36-aa1b5a28c88d");
+                        
+                const droppDownToggle = document.createElement("div");
+                droppDownToggle.classList.add("dropdown1_toggle", "w-dropdown-toggle");
+            
+                const deleteButton = document.createElement("button");
+                deleteButton.classList.add("iconButton", "is-icon-only");
+                deleteButton.innerHTML = '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3 6H5H21" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M8 6V4C8 3.46957 8.21071 2.96086 8.58579 2.58579C8.96086 2.21071 9.46957 2 10 2H14C14.5304 2 15.0391 2.21071 15.4142 2.58579C15.7893 2.96086 16 3.46957 16 4V6M19 6V19C19 19.5304 18.7893 20.0391 18.4142 20.4142C18.0391 20.7893 17.5304 21 17 21H7C6.46957 21 5.96086 20.7893 5.58579 20.4142C5.21071 20.0391 5 19.5304 5 19V6H19Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>';
+                deleteButton.addEventListener("click", () => deleteComponent(elementData));
+            
+                const editButton = document.createElement("button");
+                editButton.classList.add("iconButton", "is-icon-only");
+                editButton.innerHTML = '<svg width="fit-content" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M21 4L20.327 4.673L19 3.345L19.673 2.672C19.875 2.471 19.875 2.128 19.673 1.927L18.073 0.327C17.872 0.126 17.529 0.126 17.328 0.327L16.655 1C15.788 1.867 14.212 1.867 13.345 1L3 11.345V15H6.655L16 5.655L17.345 4.309C17.546 4.108 17.889 4.108 18.09 4.309L19.691 5.909C19.892 6.11 19.892 6.453 19.691 6.654L19 7.345L21 5.345L21 4ZM3 17H4V20H21V19H5V17H3V17Z" fill="currentColor"/></svg>';
+                editButton.addEventListener("click", () => getComponent(elementData));
+            
+                droppDownToggle.appendChild(deleteButton);
+                droppDownToggle.appendChild(editButton);
+                dropdownComponent.appendChild(droppDownToggle);
                 contentRight.appendChild(dropdownComponent);
-
                 item.appendChild(avatar);
                 item.appendChild(contentRight);
-
                 wrapper.appendChild(item);
-
-                elementList.appendChild(wrapper);
             });
+            
 
         } else {
             throw new Error('Fehler bei der Serveranfrage.');
@@ -88,8 +101,6 @@ async function getPool() {
         console.log(error)
     }
 }
-
-
 
 function deleteComponent(e) {
     pool = e
@@ -113,26 +124,26 @@ function getComponent(e) {
     console.log(e.Name)
 }
 
-let mouseIsDown = false
-cube = document.querySelectorAll(".cube-true")
+// let mouseIsDown = false
+// cube = document.querySelectorAll(".cube-true")
 
 
-self.addEventListener('mousedown', event => {
-    mouseIsDown = true
-    drawEventListener(event, true)
-})
-self.addEventListener('mouseup', event => (mouseIsDown = false))
-let drawEventListener
-document.querySelectorAll('.canvas').forEach(canvas =>
-    canvas.addEventListener('mouseover', drawEventListener = (event, toggle = false) => {
-        event.preventDefault()
-        if (mouseIsDown) {
-            const firstElement = event.composedPath()[0]
-            if (firstElement.classList.contains("cube")) {
-                firstElement.classList[toggle ? "toggle" : "add"]("selected")
-            }
-        }
-    }))
+// self.addEventListener('mousedown', event => {
+//     mouseIsDown = true
+//     drawEventListener(event, true)
+// })
+// self.addEventListener('mouseup', event => (mouseIsDown = false))
+// let drawEventListener
+// document.querySelectorAll('.canvas').forEach(canvas =>
+//     canvas.addEventListener('mouseover', drawEventListener = (event, toggle = false) => {
+//         event.preventDefault()
+//         if (mouseIsDown) {
+//             const firstElement = event.composedPath()[0]
+//             if (firstElement.classList.contains("cube")) {
+//                 firstElement.classList[toggle ? "toggle" : "add"]("selected")
+//             }
+//         }
+//     }))
 
 
 function resetTrue() {
