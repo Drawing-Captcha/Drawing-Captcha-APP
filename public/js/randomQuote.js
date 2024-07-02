e08dd72b6a8a4e0aa9403779a7c64a369ef247b9
Nukecola2005 <williams.pesic@gmail.com>
2024-07-02 11:52:39 +0200

    FEATURE: Captchas are being displayed on the dashboard for each company

--- public/js/randomQuote.js
+++ public/js/randomQuote.js
@@ -1,27 +1,27 @@
-const randomQuote = document.querySelector(".randomQuote");
-const author = document.querySelector(".randomQuoteAuthor");
-const apiUrl = "https://api.quotable.io/random";
+// const randomQuote = document.querySelector(".randomQuote");
+// const author = document.querySelector(".randomQuoteAuthor");
+// const apiUrl = "https://api.quotable.io/random";
 
-let lastUpdated = sessionStorage.getItem("lastUpdated");
+// let lastUpdated = sessionStorage.getItem("lastUpdated");
 
-if (!lastUpdated || new Date() - new Date(lastUpdated) > 86400000) {
-  getQuote();
-} else {
-randomQuote.innerText = sessionStorage.getItem("lastQuote");
-  author.innerText = sessionStorage.getItem("lastAuthor");
-}
+// if (!lastUpdated || new Date() - new Date(lastUpdated) > 86400000) {
+//   getQuote();
+// } else {
+// randomQuote.innerText = sessionStorage.getItem("lastQuote");
+//   author.innerText = sessionStorage.getItem("lastAuthor");
+// }
 
-function getQuote() {
-  fetch(apiUrl)
-    .then((data) => data.json())
-    .then((item) => {
-      randomQuote.innerText = item.content;
-      author.innerText = item.author;
-      sessionStorage.setItem("lastUpdated", new Date().toString());
-      sessionStorage.setItem("lastQuote", item.content);
-      sessionStorage.setItem("lastAuthor", item.author);
-    })
-    .catch((error) => console.error(error));
-}
+// function getQuote() {
+//   fetch(apiUrl)
+//     .then((data) => data.json())
+//     .then((item) => {
+//       randomQuote.innerText = item.content;
+//       author.innerText = item.author;
+//       sessionStorage.setItem("lastUpdated", new Date().toString());
+//       sessionStorage.setItem("lastQuote", item.content);
+//       sessionStorage.setItem("lastAuthor", item.author);
+//     })
+//     .catch((error) => console.error(error));
+// }
 
-setInterval(getQuote, 86400000);
+// setInterval(getQuote, 86400000);
