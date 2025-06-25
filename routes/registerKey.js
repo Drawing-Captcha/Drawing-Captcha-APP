46a7b5c16559f0e6cab6a38d9703c80b57f25bfb
wpesicDev <williams.pesic@gmail.com>
2025-06-25 16:29:46 +0200

    added better accountability on registerkey Endpoints

--- routes/registerKey.js
+++ routes/registerKey.js
@@ -5,13 +5,16 @@ const registerKeyModel = require("../models/RegisterKey.js");
 const userModel = require("../models/User.js");
 const sanitize = require('mongo-sanitize');
 const isAppAdmin = require("../middlewares/isAppAdmin.js");
+const createModuleLogger = require('../utils/loggerHelper');
+const logger = createModuleLogger(__filename);
+const sanitizeInput = require("../services/sanitizeInput.js");
 
 // file deepcode ignore NoRateLimitingForExpensiveWebOperation: <please specify a reason of ignoring this>
 router.get('/', hasAlreadyEnteredRegisterKeyRedirect, (req, res) => {
-    try{
+    try {
         res.render("registerKeyVerification", { error: req.query.error });
     }
-    catch(error){
+    catch (error) {
         console.error("Error occurred during test connection with client:", error);
     }
 });
@@ -21,8 +24,13 @@ router.post('/', hasAlreadyEnteredRegisterKeyRedirect, async (req, res) => {
     try {
         const findedRegisterKey = await registerKeyModel.findOne({ RegisterKey: registerKey });
         if (!findedRegisterKey) {
+            logger.warn(`Unauthorized attempt to use register key from User ${req.session.user._id}, registerKey: ${registerKey} and AppAdmin: ${req.session.user.appAdmin}`, {
+                userId: req.session.user?._id,
+                registerKey: registerKey,
+                operation: 'use_register_key_unauthorized'
+            })
             return res.status(401).redirect("/registerKey?error=Wrong+Register+Key+Please+try+again");
-            }
+        }
         const user = await userModel.findById(req.session.user._id);
         user.usedRegisterKey = true;
         user.company = findedRegisterKey.Company;
@@ -36,13 +44,21 @@ router.post('/', hasAlreadyEnteredRegisterKeyRedirect, async (req, res) => {
 });
 
 router.post('/verify', isAppAdmin, async (req, res) => {
-    const { userId } = req.body;
+    const userId = sanitizeInput(req.body.userId);
     try {
         const user = await userModel.findById(userId);
         if (!user) {
+            logger.warn(`Unauthorized attempt to verify user given: ${userId} and UserID: ${req.session.user._id} from AppAdmin ${req.session.user.appAdmin}`, {
+                userId: req.session.user?._id,
+                operation: 'verify_user_unauthorized'
+            })
             return res.status(404).json({ message: "User not found" });
         }
         if (user.usedRegisterKey) {
+            logger.warn(`Unauthorized attempt to verify user given: ${userId} and UserID: ${req.session.user._id} from AppAdmin ${req.session.user.appAdmin}`, {
+                userId: req.session.user?._id,
+                operation: 'verify_user_unauthorized'
+            })
             return res.status(400).json({ message: "User already verified" });
         }
         user.usedRegisterKey = true;
